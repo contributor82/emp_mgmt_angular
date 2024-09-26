48048a2d232784a3a9ee69b1bfccc813b18aeef0
Git Contributor <74165300+my382@users.noreply.github.com>
2024-09-26 11:42:45 +0000

    Angular 18 learning in progress

--- README.md
+++ README.md
@@ -10,3 +10,25 @@
 
 Another way to create component is
 'ng g c designation' to generate new component
+
+'ng serve' to compile and run the application. It will provide an URL to click where application will be available.
+
+Use directives in html for conditional elements display.
+
+Directives : Structural (ngIf, ngFor) & Attribute (ngClass, ngStyle)
+
+API Call
+
+To run the project on different port, use
+'ng serve --port 4209'
+
+Create a new service
+'ng g s [service name]'
+
+Using Control flow statement in HTML files
+@if..@else, @for, @switch
+
+Using Routing
+Define Routes in app.routes
+Add  <router-outlet></router-outlet> in app.Component HTML
+Import RouterLink, RouterLinkActive in App component
--- angular_18_tutorial/src/app/components/roles/roles.component.ts
+++ angular_18_tutorial/src/app/components/roles/roles.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../../model/class/interface/role';
+import { APIResponseModel } from '../../model/class/interface/apiresponsemodel';
+import { CommonModule } from '@angular/common';
 
 // In binding, there are 3 ways
 // Interpolation such as passing a variable as part of {{}}
@@ -9,29 +13,68 @@ import { FormsModule } from '@angular/forms';
 // Two-way data binding can be used only on input elements.
 //Event binding such as <input (click)="showMessage('Welcome Angular')" />
 
+// Included CommonModule for using Structurl directive.
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
-  firstName: String = 'Angular Tutorial';
-  angularVersion = "Version 18";
-  version: number = 18;
-  isActive: boolean = false;
-  currentDate: Date = new Date();
-  inputType: string = "button";
-  selectedState: string = "";
 
-  showWelcomeAlert() {
-    alert('Welcome to Angular 18 tutorial.')
+//export class RolesComponent {
+// Following code illustrate as
+// 1. How to add new variable in component class
+// 2. How to write parameterless function
+// 3. How to write function with parameter
+
+// firstName: String = 'Angular Tutorial';
+// angularVersion = "Version 18";
+// version: number = 18;
+// isActive: boolean = false;
+// currentDate: Date = new Date();
+// inputType: string = "button";
+// selectedState: string = "";
+
+// showWelcomeAlert() {
+//   alert('Welcome to Angular 18 tutorial.')
+// }
+
+// showMessage(message: string) {
+//   alert(message);
+// }
+
+//}
+
+export class RolesComponent implements OnInit {
+
+  roleList: IRole[] = [];
+  http = inject(HttpClient);
+  // Here we are injecting HttpClient dependency
+  // Using dependency injection called constructor dependency injection.
+
+  // constructor(private http: HttpClient) {
+
+  // }
+
+  ngOnInit(): void {
+    this.getAllRoles();
   }
 
-  showMessage(message: string) {
-    alert(message);
+  // API Call
+  // Currently getting CORS Policy error
+  //To fix CORS error,
+  // Added proxy.config.json
+  // Updated angular.json file section serve/development with  "proxyConfig": "src/proxy.config.json"
+  // Updated package.json file and added "start:proxy": "ng serve --proxy-config src/proxy.config.json" in serve section
+  // So far still getting CORS error causing no response getting captured.
+  getAllRoles() {
+    // To catch the data from api call,  we need to use subscribe method
+    this.http.get<APIResponseModel>('https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles').subscribe((res: APIResponseModel) => {
+      this.roleList = res.data;
+    }, error => {
+      alert('Roles cannot be loaded.');
+    });
   }
-
 }
